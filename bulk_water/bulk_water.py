74e5a89d18af04b5486476f445d1023f307739a5
Ray A. Matsumoto <ray.a.matsumoto@vanderbilt.edu>
2020-04-20 11:28:33 -0500

    Reformat atomtyping as a method of the class

--- bulk_water/bulk_water.py
+++ bulk_water/bulk_water.py
@@ -3,6 +3,7 @@ Primary function of recipe here
 """
 
 import mbuild as mb
+import parmed as pmd
 import os
 from pkg_resources import resource_filename
 from foyer import Forcefield
@@ -23,16 +24,21 @@ class build_water_box(mb.Compound):
     water_PM : Parametrized ParmEd structure
         ParmEd structure of a box of water
     """
-    def __init__(self, n_compounds=1000, forcefield='spce'):
+    def __init__(self, n_compounds=1000):
         super(build_water_box, self).__init__()
         # Build a box of water
         water = _get_water()
         water_box = mb.fill_box(water, n_compounds, density=1000)
+        self.add(water_box)
 
+    def atomtype(self, forcefield='spce'):
         # Type water system
+        print("Atomtyping with the {} force field".format(
+            forcefield))
         ff = _get_ff(forcefield)
+        water_PM = ff.apply(self)
 
-        water_PM = ff.apply(water_box)
+        return water_PM
 
 def _get_water():
     cache_dir = resource_filename('bulk_water', 'lib')
